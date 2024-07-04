bcccec637db5d1a52ef57c475b22ef9812801a13
iam-talhaa <tahakhan4141@gmail.com>
2024-07-04 19:11:14 +0500

    firebase todo App

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatefulWidget {
+  const Myapp({super.key});
+
+  @override
+  State<Myapp> createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("TESTIN"),
+        ),
+      ),
+    );
+  }
+}
